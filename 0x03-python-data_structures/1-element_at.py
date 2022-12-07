25b06e7f176312e83218f763a670377c8109cd63
Adetoroye Oluleke Moses <adetleke@gmail.com>
2022-12-07 03:13:44 +0000

    ..

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-def element_at(,y_list, idx):
+def element_at(my_list, idx):
     if idx < 0 or >= len(my_list):
         return ("None")
     else:
-
         return my_list[idx]
